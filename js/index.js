6354679d6c5b3e3912a1adbc15b635b3c1fa8b51
olazajac <zaioncola@gmail.com>
2017-07-23 14:13:40 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $header_top.find('a').on('click', function() {
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionsColor: ['#cc3b35', '#cc3b35', '#cc3b35', '#cc3b35', '#cc3b35'],
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,
